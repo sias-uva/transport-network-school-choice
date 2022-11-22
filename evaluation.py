d656a3c608577b4a85210b4cf97ac241feb3352b
dimichai <dimitris@hey.com>
2022-11-22 17:03:16 +0100

    facility_capacity docstring

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def facility_capacity(population, facilities, allocation):
+    """Returns an array of percentage of satisfied capacity per facility.
+
+    Args:
+        population (pd.DataFrame): population dataframe - should have id column
+        facilities (pd.DataFrame): facility dataframe - should have id column
+        allocation (np.array): array of shape (total_pop, 1) where each agent is allocated to 1 facility.
+
+    Returns:
+        array: array of satisfied capacity per facility.
+    """
     assert allocation.shape[1] == 1, 'Only one facility should be allocated to each agent.'
     
     eval = []
