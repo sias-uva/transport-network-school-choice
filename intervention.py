1d4905baaf15d8bb1996876ca81b888bfb5edeb5
dimichai <dimitris@hey.com>
2023-01-20 17:41:41 +0100

    intervention.py random now does not break if no interventions can be done

--- intervention.py
+++ intervention.py
@@ -37,7 +37,9 @@ def create_random_edge(network: Network, edge_weight=1):
     adj_mx = network.get_adj_matrix()
     # Get the indices of the non-connected nodes in the adjacecy matrix.
     candidate_idx = np.where(adj_mx == 0)
-    assert len(candidate_idx[0]) > 0, 'Cannot add more edges, all nodes are connected.'
+    if len(candidate_idx[0]) <= 0:
+        return None, None, None
+    # assert len(candidate_idx[0]) > 0, 'Cannot add more edges, all nodes are connected.'
     # Get a random index from the non-connected nodes.
     rand_idx = np.random.choice(candidate_idx[0].shape[0])
     x = candidate_idx[0][rand_idx]
