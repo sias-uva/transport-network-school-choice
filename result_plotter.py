5cff507a3bcb1760ebf652f014c8ca0e83a44140
dimichai <dimitris@hey.com>
2023-02-13 11:12:02 +0100

    result_plotter.py now plots everything in one figure

--- result_plotter.py
+++ result_plotter.py
@@ -4,10 +4,12 @@ import numpy as np
 from pathlib import Path
 from plot import get_figure
 import matplotlib.pyplot as plt
+import yaml
 
 plt.rcParams.update({'font.size': 22})
-SUBTITLE_FONT_SIZE = 18
-LEGEND_FONT_SIZE = 14
+TITLE_FONT_SIZE = 28
+SUBTITLE_FONT_SIZE = 26
+LEGEND_FONT_SIZE = 16
 
 def plot_results(results, ax):
     # Names of the results
@@ -24,11 +26,8 @@ def plot_results(results, ax):
     for result in results:
         diss_ci.append(np.loadtxt(result_path / f'{result}/dissimilarity_index.txt', delimiter=','))
 
-        d = {}
         with open(result_path / f'{result}/output.txt') as f:
-            for line in f:
-                (key, val) = line.split(':')
-                d[key] = val.strip()
+            d = yaml.safe_load(f)
         output_files.append(d)
 
     # fig, ax_ = get_figure(f"Average Dissimilarity Index in Schools",
@@ -37,13 +36,16 @@ def plot_results(results, ax):
     #                     ylabel='Dissimilarity Index',
     #                     figsize=(12, 7.5),
     #                     ylim=(0, 0.8))
-    ax.set_title(f'{diss_ci[1].shape[0]} simulation rounds, {output_files[1]["intervention_rounds"]} intervention rounds', color='gray', fontsize=SUBTITLE_FONT_SIZE)
+
 
     for i in range(len(results)):
         sim_rounds = diss_ci[i].shape[0]
 
-        ax.plot(range(sim_rounds), diss_ci[i][:, 0], line_styles[i], label=f'{names[i]}', color=colors[i], linewidth=2)
+        ax.plot(range(sim_rounds), diss_ci[i][:, 0], line_styles[i], label=f'{names[i]}', color=colors[i], linewidth=3)
         ax.fill_between(range(sim_rounds), diss_ci[i][:, 1], diss_ci[i][:, 2], color=f'C{i}', alpha=.1)
+        
+        for r in output_files[i]['rounds_with_intervention']:
+                ax.axvline(r, linestyle='dashed', linewidth=1, color='#C2C2C2', alpha=0.1)
 
     return ax
     # fig.legend(loc='lower left', fontsize=LEGEND_FONT_SIZE, bbox_to_anchor=(0, -.05), ncol=2)
@@ -78,13 +80,20 @@ fig, ax = plt.subplots(1, 2, figsize=(24, 7.5))
 #                         figsize=(12, 7.5),
 #                         ylim=(0, 0.8))
 
-plot_results(results_ams, ax[0])
-plot_results(results_sbm, ax[1])
+plot_results(results_sbm, ax[0])
+plot_results(results_ams, ax[1])
+ax[0].set_ylim(0, 0.8)
+ax[1].set_ylim(0, 0.8)
 ax[0].set_xlabel('Simulation round')
 ax[0].set_ylabel('Disimilarity Index')
 ax[1].set_xlabel('Simulation round')
 ax[1].set_ylabel('Disimilarity Index')
+ax[0].set_title(f'(A) Synthetic Environment', fontsize=SUBTITLE_FONT_SIZE)
+ax[1].set_title(f'(B) Amsterdam Environment', fontsize=SUBTITLE_FONT_SIZE)
 
+# fig.suptitle('Impact of Network Interventions on Segregation (TODO ADD GROUP BETWEENNESS)', fontsize=TITLE_FONT_SIZE)
+handles, labels = ax[0].get_legend_handles_labels()
+plt.figlegend(handles, labels, loc = 'lower center', ncol=8, labelspacing=0., bbox_to_anchor=(0.5, -0.15))
 fig.show()
 
 # %%
