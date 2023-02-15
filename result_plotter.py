4976c2183af539f9952ed294dca61b68fbde6e9b
dimichai <dimitris@hey.com>
2023-02-15 17:41:00 +0100

    result_plotter full results

--- result_plotter.py
+++ result_plotter.py
@@ -2,7 +2,6 @@
 # It is therefore meant to be used as a script and not as a module.
 import numpy as np
 from pathlib import Path
-from plot import get_figure
 import matplotlib.pyplot as plt
 import yaml
 
@@ -11,13 +10,14 @@ TITLE_FONT_SIZE = 28
 SUBTITLE_FONT_SIZE = 26
 LEGEND_FONT_SIZE = 16
 
+#%% Plot the simulation results side by side
 def plot_results(results, ax):
     # Names of the results
     # names = ['none', 'random', 'closeness', 'betweenness', 'degree', 'group_closeness', 'group_betweenness', 'group_degree']
-    names = ['none', 'random', 'closeness', 'group_closeness', 'betweenness', 'degree', 'group_degree']
-    colors = ['gray', 'gray', '#e60049', '#e60049', '#0bb4ff', '#50e991', '#50e991']
+    names = ['none', 'random', 'closeness', 'group_closeness', 'betweenness', 'group_betweenness', 'degree', 'group_degree']
+    colors = ['gray', 'gray', '#e60049', '#e60049', '#0bb4ff', '#0bb4ff', '#50e991', '#50e991']
     # line_styles = ['--', '--', '--', '--', '--', '-', '-', '-']
-    line_styles = ['-.', '--', '--', '-', '--', '--', '-']
+    line_styles = ['-.', '--', '--', '-', '--', '-', '--', '-']
     result_path = Path('./results')
 
     # Confidence intervals of dissimilarity index
@@ -57,6 +57,7 @@ results_ams = [
         '20230213_11_23_16.596338_amsterdam_neighborhoods_50_5_25_1_distance_composition_random_serial_dictatorship_closeness',
         '20230213_11_28_21.533442_amsterdam_neighborhoods_50_5_25_1_distance_composition_random_serial_dictatorship_group_closeness',
         '20230213_11_23_27.183322_amsterdam_neighborhoods_50_5_25_1_distance_composition_random_serial_dictatorship_betweenness',
+        '20230213_15_26_16.628397_amsterdam_neighborhoods_50_5_25_1_distance_composition_random_serial_dictatorship_group_betweenness',
         '20230213_11_24_02.853325_amsterdam_neighborhoods_50_5_25_1_distance_composition_random_serial_dictatorship_degree',
         '20230213_11_28_34.792581_amsterdam_neighborhoods_50_5_25_1_distance_composition_random_serial_dictatorship_group_degree'
         ]
@@ -67,6 +68,7 @@ results_sbm = [
         '20230210_17_20_38.371774_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_random',
         '20230210_17_19_45.202179_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_closeness',
         '20230210_17_21_39.629291_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_group_closeness',
+        '20230210_17_22_09.075038_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_group_betweenness',
         '20230210_17_21_20.236519_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_betweenness',
         '20230210_17_21_27.596022_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_degree',
         '20230210_17_22_25.239575_sbm_50_5_2_1_distance_composition_random_serial_dictatorship_group_degree',
@@ -85,15 +87,13 @@ plot_results(results_ams, ax[1])
 ax[0].set_ylim(0, 0.8)
 ax[1].set_ylim(0, 0.8)
 ax[0].set_xlabel('Simulation round')
-ax[0].set_ylabel('Disimilarity Index')
+ax[0].set_ylabel('Dissimilarity Index')
 ax[1].set_xlabel('Simulation round')
-ax[1].set_ylabel('Disimilarity Index')
+ax[1].set_ylabel('Dissimilarity Index')
 ax[0].set_title(f'(A) Synthetic Environment', fontsize=SUBTITLE_FONT_SIZE)
 ax[1].set_title(f'(B) Amsterdam Environment', fontsize=SUBTITLE_FONT_SIZE)
 
 # fig.suptitle('Impact of Network Interventions on Segregation (TODO ADD GROUP BETWEENNESS)', fontsize=TITLE_FONT_SIZE)
 handles, labels = ax[0].get_legend_handles_labels()
-plt.figlegend(handles, labels, loc = 'lower center', ncol=8, labelspacing=0., bbox_to_anchor=(0.5, -0.15))
+plt.figlegend(handles, labels, loc = 'lower center', ncol=4, labelspacing=0., bbox_to_anchor=(0.5, -0.15))
 fig.show()
-
-# %%
